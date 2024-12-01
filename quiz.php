37c205895cc9a8b5a3773b7b0e0c4bd21a36d523
Imee Vito <vitoimee@gmail.com>
2024-12-01 17:32:23 +0800

    Added quiz.php HTML

--- quiz.php
+++ quiz.php
@@ -5,17 +5,20 @@
 <style>
 body {background-color: powderblue;
     justify-content: center;
-    align-content: center;
     display: flex;
     padding: 10%;}
 h1   {color: blue;}
+h2   {color: blue;}
 p    {color: red;}
 .box1{background-color: white;
     border-radius: 12px; 
     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
     border: none;
     padding: 20px;
-    width: 50%;}
+    width: 50%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;}
 </style>
 </head>
 <body>
@@ -25,10 +28,24 @@ session_start();
 
 <div class = "box1">
     <div>
-        <h3>Score</h3>
-        <h4>Correct: Wrong: </h4>
+        <h2>Score</h2>
+        <h4>Correct: </h4>
+        <h4>Wrong: </h4>
+        <a href= index.php>Start Again</a><br><br>
+        <a href=index.php>Close Quiz</a>
     </div>
-    <a href= index.php>Start Again</a>
+    <div style="margin-right:220px;">
+        <h1>Math Quiz</h1>
+        <form method="post" action="quiz.php">
+            <p>Question: </p>
+            <input type="radio" name="ans" value="" required> A.<br>
+            <input type="radio" name="ans" value="" required> B.<br>
+            <input type="radio" name="ans" value="" required> C.<br>
+            <input type="radio" name="ans" value="" required> D.<br><br>
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 </div>
 
 </body>
